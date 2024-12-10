d3ae1dc8ed21a3859608c3dd36d49f0d53b952f7
StalinTumbaco <stalin2005tumbaco@gmail.com>
2024-12-10 18:59:16 -0500

    cuarta cambio

--- djangoevents/urls.py
+++ djangoevents/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from eventos import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', include('eventos.urls')),
 ]
