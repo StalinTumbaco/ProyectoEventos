2d963169bae4d03c26dd1a98511f7c92ce1b1bae
JMDC <majin325yt@gmail.com>
2024-12-29 17:16:07 -0500

    Solucion para admin temporal

--- build.sh
+++ build.sh
@@ -8,5 +8,13 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
+"
+
+
 # Apply any outstanding database migrations
 python manage.py migrate
