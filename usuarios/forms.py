1a3d76b5c830e8e45dab02d6f5a1caa319fc6e54
StalinTumbaco <stalin2005tumbaco@gmail.com>
2024-12-29 01:47:11 -0500

    Agrege lo de validar la identificacion y estilize los templates de servicio y login y registro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -17,7 +17,14 @@ class RegistroForm(UserCreationForm):
         ]
 
         widgets = {
-            'genero': forms.Select(choices=[
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de usuario'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}),
+            'identificacion_cliente': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Identificación'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Teléfono'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Correo electrónico'}),
+            'nacionalidad': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nacionalidad'}),
+            'genero': forms.Select(attrs={'class': 'form-select'}, choices=[
                 ('Masculino', 'Masculino'),
                 ('Femenino', 'Femenino'),
                 ('No binario', 'No binario'),
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
 class Usuario(AbstractUser):
     # Campos adicionales que no están en el modelo User por defecto
@@ -8,6 +9,13 @@ class Usuario(AbstractUser):
     fecha_registro = models.DateField(auto_now_add=True)
     telefono = models.CharField(max_length=15, blank=True, null=True)
     genero = models.CharField(max_length=20, blank=True, null=True)
+
+    def clean(self):
+        super().clean()
+        if not self.identificacion_cliente.isdigit():
+            raise ValidationError({'identificacion_cliente': 'La identificación debe contener solo números.'})
+        if len(self.identificacion_cliente) != 10:
+            raise ValidationError({'identificacion_cliente': 'La identificación debe tener exactamente 10 dígitos.'})
 
     def __str__(self):
         return f"{self.username} - {self.identificacion_cliente}"
